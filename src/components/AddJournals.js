50c1d8445eb52c60507cafa8befc923b194e37b6
ArunRaj7488 <arunraj1735169@gmail.com>
2020-08-23 02:30:29 +0530

    fixes css and add id filed and sorting data

--- src/components/AddJournals.js
+++ src/components/AddJournals.js
@@ -1,102 +1,143 @@
-import React,{ Component } from 'react';
-//import UploadServices from '../services/UploadFiles';
-import { Button, Row, Col } from 'react-bootstrap';
-import '../../src/Css/addJournals.css';
+import React, { Component } from "react";
+import "../../src/Css/addJournals.css";
 
 class AddJournals extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // formData: {
-              headLine: '',
-              description: '',
-              newDate: '',
-              image: [null]
-              
-            // },
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      // formData: {
+        id: '',
+      headLine: "",
+      description: "",
+      newDate: "",
+      image: [null],
+
+      // },
     };
+  }
 
-    handleChange = (e)=>{
-        if(e.currentTarget.name === "headLine") this.setState({headLine: e.currentTarget.value});
-        if(e.currentTarget.name === "description") this.setState({description: e.currentTarget.value});
-       // if(e.currentTarget.name === "newDate") this.setState({newDate: e.currentTarget.value})
-    }
+  handleChange = (e) => {
+    if (e.currentTarget.name === "headLine")
+      this.setState({ headLine: e.currentTarget.value });
+    if (e.currentTarget.name === "description")
+      this.setState({ description: e.currentTarget.value });
+    if (e.currentTarget.name === "newDate")
+      this.setState({ newDate: e.currentTarget.value });
+  };
 
-     onSubmit = async (e) => {
-      try {
-        e.preventDefault();
+  onSubmit = async (e) => {
+    try {
+      e.preventDefault();
 
-        let dataObject = {
-          headLine: this.state.headLine,
-          description: this.state.description,
-          newDate: new Date(),
-        };
-
-        let previousData = []
-      
-        if(JSON.parse(localStorage.getItem("journalData"))) {
-          previousData = JSON.parse(localStorage.getItem("journalData"));
-          console.log("gdhsgdhs",previousData);
-          previousData.push(dataObject)
-        }
-        else{
-          previousData.push(dataObject)
-          console.log("2",previousData);
-
-        }
-        console.log("3",previousData);
-
-
-        //previousData.map( item => {
-          let res = localStorage.setItem('journalData',JSON.stringify(previousData) );
-        //})
-        window.location.pathname = "/"
-
-
-        alert("Add Journal Data successfully");
-      } catch (err) {
-        console.log('Something went wrong!', err)
+      if (this.state.headLine === "") {
+        alert("Headline can not be empty!");
+        return;
       }
-    };
+      if (this.state.description === "") {
+        alert("Description can not be empty!");
+        return;
+      }
+      if (this.state.newDate === "") {
+        alert("Date can not be empty!");
+        return;
+      }
+      let previousData = [];
 
-    
-    
-  
-      render () {
-          return (
-            <div className="container">
-                <form >
+      previousData = JSON.parse(localStorage.getItem("journalData"));
 
-                     <div className="form-group"   >
-                            <label className="label">HeadLines:</label>
-                            <input className="input" name="headLine" type="text" placeholder="Enter Head Line" onChange={(e)=>this.handleChange(e)} />
-                     </div>
+      let dataObject = {
+        id: previousData ? previousData.length + 1 : 1,
+        headLine: this.state.headLine,
+        description: this.state.description,
+        newDate: this.state.newDate,
+      };
 
-                     <div  className="form-group" >
-                        <label className="label" >Description:</label>
-                        <textarea  className="input" rows="4" name="description" type="textarea" placeholder="Enter Description"  onChange={(e)=>this.handleChange(e)} />
-                     </div>
+      console.log(previousData, dataObject );
+      // let previousData = [];
+      // previousData = JSON.parse(localStorage.getItem("journalData"));
 
-                     {/* <div  className="form-group" >
-                        <label className="label" >Add Date:</label>
-                        <input className="input" style={{}} name="newDate" type="date" placeholder="Enter Date"  onChange={(e)=>this.handleChange(e)} />
-                     </div>
+      // if (JSON.parse(localStorage.getItem("journalData"))) {
+      //   console.log("gdhsgdhs", previousData);
+      //   previousData.push(dataObject);
+      // } else {
+      // }
 
-                     <div  className="form-group" >
+    if(previousData){
+      previousData.push(dataObject);
+      let res = localStorage.setItem(
+        "journalData",
+        JSON.stringify(previousData)
+      );
+    }else{
+      let res = localStorage.setItem(
+        "journalData",
+        JSON.stringify([dataObject])
+      );
+    }
+      
+      //})
+      window.location.pathname = "/";
+
+      alert("Add Journal Data successfully");
+    } catch (err) {
+      console.log("Something went wrong!", err);
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h3>Add Journal Form</h3>
+        <form className="form">
+          <span className="form-group">
+            <label className="label">HeadLines:</label>
+            <input
+              required
+              className="input"
+              name="headLine"
+              type="text"
+              placeholder="Enter Head Line"
+              onChange={(e) => this.handleChange(e)}
+            />
+          </span>
+
+          <span className="form-group">
+            <label className="label">Description:</label>
+            <textarea
+              className="input"
+              rows="4"
+              name="description"
+              type="textarea"
+              placeholder="Enter Description"
+              onChange={(e) => this.handleChange(e)}
+            />
+          </span>
+
+          <span className="form-group">
+            <label className="label">Add Date:</label>
+            <input
+              className="input"
+              style={{}}
+              name="newDate"
+              type="date"
+              placeholder="Enter Date"
+              onChange={(e) => this.handleChange(e)}
+            />
+          </span>
+
+          {/* <div  className="form-group" >
                         <label className="label" >Add Image:</label>
                         <input className="input" style={{}} name="image" type="file" placeholder="Enter Date"  onChange={(e)=>this.handleChange(e)} />
                      </div> */}
 
-                     <div as={Row}className="form-group" >
-                        <Col sm={{ span: 10, offset: 2 }}>
-                        <Button onClick={this.onSubmit}>Add New Journal</Button>
-                        </Col>
-                    </div>
-
-                </form>
-            </div>
-          );
-      }
-};
-export default AddJournals
+          <div style={{ marginRight: "20px" }}>
+            <button className="btn btn-primary" onClick={this.onSubmit}>
+              Add New Journal
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default AddJournals;
--- src/components/cadrViewPage.js
+++ src/components/cadrViewPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import moment from "moment";
 import "../Css/cardViewPage.css";
+import _ from "loadsh";
 
 class CardViewPage extends Component {
   constructor(props) {
@@ -13,14 +14,19 @@ class CardViewPage extends Component {
   componentDidMount() {
     let Journal = JSON.parse(localStorage.getItem("journalData"));
     console.log("Journal", Journal, typeof Journal);
-    if(  Journal){
-     const sortedActivities = Journal.slice().sort((a, b) => new Date(a.newDate) - new Date(b.newDate));
+    let sortData = [];
+    if (Journal && Journal.length > 1) {
+      sortData = _.sortBy(Journal, function (object) {
+        console.log(moment(object.newDate).format());
+        return moment(object.newDate).format();
+      }).reverse();
+      this.setState({ JournalData: sortData });
 
-     console.log(sortedActivities);
+    } else{
+      this.setState({ JournalData: Journal });
+
     }
 
-
-    this.setState({ JournalData: Journal });
 
     console.log(
       "data",
@@ -36,30 +42,39 @@ class CardViewPage extends Component {
           <h1> All Journal</h1>
         </div>
         <div className="btn-add">
-          <a className="btn btn-primary " href="/add-journal">Add New Journal</a>
+          <a className="btn btn-primary " href="/add-journal">
+            Add New Journal
+          </a>
         </div>
-        <div className="container">
-          {this.state.JournalData && this.state.JournalData.map((journal,i) => (
-            <div className="cardContainer" key={journal.newDate, i+3} >
-              <div className="date">
-                <h5 key={journal.newDate, i}className="font-style">
-                  {moment(journal.newDate).format("dddd")}
-                </h5>
-                <h5 key={journal.newDate, i+1}>{moment(journal.newDate).format("DD-MMM-YYYY")}</h5>
+        <div className="container-view">
+          {this.state.JournalData &&
+            this.state.JournalData.map((journal, i) => (
+              <div className="cardContainer" key={(journal && journal.newDate, i + 3)}>
+                <div className="date">
+                  <h5 key={(journal && journal.newDate, i)} className="font-style">
+                    {moment(journal && journal.newDate).format("dddd")}
+                  </h5>
+                  <h5 key={(journal && journal.newDate, i + 1)}>
+                    {moment(journal && journal.newDate).format("DD-MMM-YYYY")}
+                  </h5>
+                </div>
+                <div className="data-field">
+                  <h1 key={journal && journal.headLine} className="headline">
+                    {journal && journal.headLine}
+                  </h1>
+                  <p key={journal && journal.description} className="description">
+                    {journal && journal.description}
+                  </p>
+                </div>
+                <div className="image-div">
+                  <img
+                    src="http://picsum.photos/100/100"
+                    style={{ borderRadius: "5px" }}
+                    alt="img"
+                  />
+                </div>
               </div>
-              <div className="data-field">
-                <h1 key={journal.headLine}>{journal.headLine.slice(0, 30) + "..."}</h1>
-                <p key={journal.description}>{journal.description.slice(0, 100) + "..."}</p>
-              </div>
-              <div className="image-div">
-                <img
-                  src="http://picsum.photos/100/100"
-                  style={{ borderRadius: "5px" }}
-                  alt="img"
-                />
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     );
